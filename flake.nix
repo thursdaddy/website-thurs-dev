f28c77ea9467aea45ec6151fdcec873b4bac0c43
thurs <thursdaddy@pm.me>
2025-01-09 21:21:35 -0700

    init

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Hugo Flake.nix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem
+        ({ pkgs }: {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.awscli2
+              pkgs.hugo
+              pkgs.go
+              pkgs.dart-sass
+              pkgs.nodePackages.nodejs
+              pkgs.nodePackages.npm
+            ];
+          };
+        });
+    };
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/thursdaddy/website-thurs-dev
+
+go 1.23.3
+
+require github.com/hugo-sid/hugo-blog-awesome v1.19.0 // indirect
